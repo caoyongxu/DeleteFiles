d43cca6fa308b8a8097bbed8b6de378bc900ba1b
caoyongxu <caoyongxu@163.com>
2019-03-29 16:29:48 +0800

    save project

---
+++ Solution1/DeleteFiles/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.IO;
+using System.Reflection;
+
+namespace DeleteFiles
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var exeFile = Assembly.GetExecutingAssembly().CodeBase.Replace("file:///", ""); ;
+            var currntfolder = Path.GetDirectoryName(exeFile);
+
+            List<DirectoryInfo> Folders = new List<DirectoryInfo>();
+            foreach (var folder in Directory.EnumerateDirectories(currntfolder))
+            {
+                Folders.Add(new DirectoryInfo(folder));
+            }
+
+            Folders.OrderBy(o => o.CreationTime);
+
+            foreach (var f in Folders)
+            {
+                System.Console.WriteLine($"{f.Name}  :  {f.CreationTime} ");
+            }
+
+            List<DirectoryInfo> Folders2Delete = Folders.Where(o => o.CreationTime < DateTime.Now.AddMonths(-7)).ToList();
+
+            foreach (var fd in Folders2Delete)
+            {
+                try
+                {
+                    fd.Delete(true);
+                    System.Console.WriteLine($"Deleted the folder: {fd}");
+                }
+                catch (Exception ex)
+                {
+                    System.Console.WriteLine(ex.ToString());
+                    //throw;
+                }               
+
+            }
+
+        }
+    }
+}
